95f6da052b643932aca98c0d6ed99e0ca38248fe
Ramyrashad127 <ramy41599@gmail.com>
2024-10-24 19:52:17 +0300

    new

---
+++ main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    video = models.FileField(upload_to='videos/')
+
+    def __str__(self):
+        return self.title
+
+
+class UserCourse(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} enrolled in {self.course.title}"
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
